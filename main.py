1722466b8fb33876336b3326f32f654e9da91a79
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-12 23:24:19 +0600

    removed unwanted comment

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Add CORS middleware to allow frontend requests
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React dev server
+    allow_origins=["http://localhost:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
